464d95af0b91f82ee0885036d0ac4f64774ec357
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-07-28 22:43:57 +0200

    Reservation form

--- js/script.js
+++ js/script.js
@@ -49,4 +49,58 @@ burger.addEventListener('click', () => {
     body.classList.toggle('locked');
 })
 
-console.log(burger)
+
+function handleSubmit(e){
+    e.preventDefault();
+
+    // Retrieve form values
+    const name = document.querySelector('.input_name').value;
+    const email = document.querySelector('.input_name[type="email"]').value;
+    const persons = document.querySelector('.input_order:nth-of-type(1)').value;
+    const timing = document.querySelector('.input_order:nth-of-type(2)').value;
+    const date = document.querySelector('.input_order:nth-of-type(3)').value;
+
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Persons:", persons);
+    console.log("Timing:", timing);
+    console.log("Date:", date);
+
+    // Create an object to hold the reservation data
+    const reservationData = {
+        name: name,
+        email: email,
+        persons: persons,
+        timing: timing,
+        date: date
+    };
+
+    // Make an AJAX request to the server using the fetch API
+    fetch('https://example.com/api/reservations', {
+        method: 'POST', // Use the HTTP method appropriate for your server endpoint
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(reservationData)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // If you expect a JSON response from the server
+        })
+        .then(data => {
+            // Handle the server's response data here (if applicable)
+            console.log('Reservation successful:', data);
+            // You can show a success message to the user or redirect to a thank-you page
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle any errors that occurred during the AJAX request
+            // You can show an error message to the user in this case
+        });
+}
+
+// Add a submit event listener to the form to handle form submission
+const reservationForm = document.getElementById('reservationForm');
+reservationForm.addEventListener('submit', handleSubmit);
